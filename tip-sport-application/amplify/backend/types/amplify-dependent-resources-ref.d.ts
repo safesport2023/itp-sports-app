1bfbc1f7047bf473fc7685606f44ef07105b2b3d
Nicholas Hoban <nickslaptop@Nicholass-MacBook-Pro.local>
2023-08-30 10:56:11 -0400

    first large push of this iteration

---
+++ tip-sport-application/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "tipsportapplicationec2f4db1": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
